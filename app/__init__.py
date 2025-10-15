353abb8d10fa7906b6234ce7ba96b95a88044924
madisonfleming <maddyyfleming@gmail.com>
2025-10-15 11:39:11 +1100

    add amenity namespace to __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_restx import Api
 from app.api.v1.users import api as users_ns
+from app.api.v1.amenities import api as amenity_name_space
 
 def create_app():
     app = Flask(__name__)
@@ -8,4 +9,6 @@ def create_app():
 
     # Register the users namespace
     api.add_namespace(users_ns, path='/api/v1/users')
+    # Register amenity namespace
+    api.add_namespace(amenity_name_space, path='/api/v1/amenities')
     return app
