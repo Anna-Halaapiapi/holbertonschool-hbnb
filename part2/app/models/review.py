2e2e126ed9a9c1fdf1f195c4aef7b7d6fd7d874b
cipher <ciphershadow1@gmail.com>
2025-11-04 11:15:07 +1100

    update review.py with property setters - based on QA review

--- part2/app/models/review.py
+++ part2/app/models/review.py
@@ -1,9 +1,10 @@
 from .base_model import BaseModel
 from .user import User
+from .place import Place
 
 class Review(BaseModel):
-    """ This module implements the Review logic
-    """
+    """ This module represents a review for a Place - written by a User"""
+
     def __init__(self, text, rating, place, user):
         super().__init__()
 
@@ -12,20 +13,53 @@ class Review(BaseModel):
         self.place = place
         self.user = user
 
-        self.validate() # Validate attribute values
-
-    def validate(self):
-        if self.text is None or not isinstance(self.text, str):
-            raise ValueError("content of review is required")
-
-        if not isinstance(self.rating, int) or not (1 <= self.rating <= 5):
-            raise ValueError("Rating must be within 1 to 5")
-
-        from .place import Place
-        if not self.place or not isinstance(self.place, Place): # must be a Place instance being reviewed
-            raise ValueError("Place instance must exist")
-                
-        if not self.user or not isinstance(self.user, User): # must be a User instance leaving review
-            raise ValueError("User instance must exist")
+        # Link review to place
+        self.place.add_review(self)
 
 
+    # -- Text attribute / validation -- 
+    @property
+    def text(self):
+        return self._text
+
+    @text.setter
+    def text(self, value):
+        if not isinstance(value, str) or not value.strip():
+            raise ValueError("Review text is required.")
+        self._text = value.strip()
+
+
+    # -- Rating attribute / validation --
+    @property
+    def rating(self):
+        return self._rating
+
+    @rating.setter
+    def rating(self, value):
+        if not isinstance(value, int) or not (1 <= value <= 5):
+            raise ValueError("Rating must be an integer between 1 and 5.")
+        self._rating = value
+
+
+    # -- Place attribute / validation --
+    @property
+    def place(self):
+        return self._place
+
+    @place.setter
+    def place(self, value):
+        if not isinstance(value, Place):
+            raise TypeError("A Place instance must exist.")
+        self._place = value
+
+
+    # -- User attribute / validation --
+    @property
+    def user(self):
+        return self._user
+
+    @user.setter
+    def user(self, value):
+        if not isinstance(value, User):
+            raise TypeError("A User instance must exist.")
+        self._user = value
