99d6b77368c45c2e3d98c7fb78bcc207bb3252b1
Anna-Halaapiapi <annahalaapiapi@hotmail.com>
2025-11-06 20:40:25 +1100

    splitting sql files

--- part3/tables.sql
+++ part3/tables.sql
@@ -1,10 +1,11 @@
--- Scripts for table creation and initial data insert
+-- Scripts for db, table creation and initial data insert
+--DATABASE CREATION
 CREATE DATABASE IF NOT EXISTS hbnb_db;
 USE hbnb_db;
 
 -- TABLE CREATION
 -- Create User Table
-CREATE TABLE `users` (
+CREATE TABLE IF NOT EXISTS `users` (
     `id` CHAR(36),
     `first_name` VARCHAR(255),
     `last_name` VARCHAR(255),
@@ -15,7 +16,7 @@ CREATE TABLE `users` (
 );
 
 -- Create Place Table
-CREATE TABLE `places` (
+CREATE TABLE IF NOT EXISTS `places` (
     `id` CHAR(36),
     `title` VARCHAR(255),
     `description` TEXT,
@@ -28,7 +29,7 @@ CREATE TABLE `places` (
 );
 
 -- Create Review Table
-CREATE TABLE `reviews` (
+CREATE TABLE IF NOT EXISTS `reviews` (
     `id` CHAR(36),
     `text` TEXT,
     `rating` INT CHECK (`rating` BETWEEN 1 AND 5),
@@ -41,37 +42,17 @@ CREATE TABLE `reviews` (
 );
 
 -- Create Amenity Table
-CREATE TABLE `amenities` (
+CREATE TABLE IF NOT EXISTS `amenities` (
     `id` CHAR(36),
     `name` VARCHAR(255) UNIQUE,
     PRIMARY KEY(`id`)
 );
 
 -- Create Place_Amenity Table
-CREATE TABLE `place_amenity` (
+CREATE TABLE IF NOT EXISTS `place_amenity` (
     `place_id` CHAR(36),
     `amenity_id` CHAR(36),
     PRIMARY KEY (`place_id`, `amenity_id`),
     FOREIGN KEY (`place_id`) REFERENCES `places` (`id`),
     FOREIGN KEY (`amenity_id`) REFERENCES `amenities` (`id`)
 );
-
--- INITIAL DATA INSERT
--- Administrator User, p/w is bcrypt hashed using 12 rounds
-INSERT into `users`
-VALUES (
-    '36c9050e-ddd3-4c3b-9731-9f487208bbc1',
-    'admin@hbnb.io',
-    'Admin',
-    'HBnB',
-    '$2a$12$J34myE4s28hOlXG8NwUfF.5V9tj/js.QasGVDgBqJjs2RdT1zm0mm',
-    TRUE
-    );
-
--- Initial Amenities
--- UUID v4 tool used to gen ids
-INSERT into `amenities`
-VALUES
-    ('e26d5d10-c86d-4e9a-98f6-5cef8508f1e2', 'WiFi'),
-    ('d5908763-ca8f-4988-816e-26756f11d86c', 'Swimming Pool'),
-    ('fe30f80c-34f8-4a11-a3d8-c7aaf374e649', 'Air Conditioning');
