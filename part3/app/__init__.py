b5e3f9a1c0cfb2309730842644745f5720d295c5
Anna-Halaapiapi <annahalaapiapi@hotmail.com>
2025-11-07 13:09:56 +1100

    update __init__.py for task 5

--- part3/app/__init__.py
+++ part3/app/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_restx import Api
+from flask_sqlalchemy import SQLAlchemy
 from app.api.v1.users import api as users_ns
 from app.api.v1.places import api as places_ns
 from app.api.v1.amenities import api as amenity_name_space
@@ -8,11 +9,14 @@ from app.extensions import bcrypt
 from app.api.v1.auth import api as auth_ns
 from app.extensions import bcrypt, jwt
 
+db = SQLAlchemy()
+
 def create_app(config_class="config.DevelopmentConfig"):
     app = Flask(__name__)
     app.config.from_object(config_class)
     bcrypt.init_app(app)
     jwt.init_app(app)
+    db.init_app(app)
     app.config["SECRET_KEY"] = "your-super-secret-key"
 
 
