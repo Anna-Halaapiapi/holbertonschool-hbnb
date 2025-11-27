d56a0d52ce43a2a5245f41d7bf70ee59975a8ce1
Anna-Halaapiapi <annahalaapiapi@hotmail.com>
2025-11-27 17:58:22 +1100

    add images in JS for place tiles

--- part4/scripts_OLD.js
+++ part4/scripts_OLD.js
@@ -177,26 +177,11 @@ function displayPlaces(places) {
   if (!placesList) return;
   placesList.innerHTML = '';
 
-  //add image list for loop
-  const images = [
-    "images/place1.png",
-    "images/place2.png",
-    "images/place3.png",
-    "images/place4.png",
-    "images/place5.png",
-    "images/place6.png"
-  ];
-
-  places.forEach((place, index) => {
+  places.forEach(place => {
     const card = document.createElement('div');
     card.className = 'place-card';
     card.setAttribute('data-price', place.price);
-    const imgSrc = images[index];
     card.innerHTML = `
-      <div class="place-image-container">
-        <img src="${imgSrc}" class="place-image" alt="${place.title}">
-      </div>
-
       <h3>${place.title}</h3>
       <p>Price: $${place.price}/night</p>
       <p>${place.description}</p>
